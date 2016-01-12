f6b07e520cfad77161e22ce71a13a5c5a7d75add
Daniel Hitzel <d-hitzel@outlook.com>
2016-01-12 10:32:02 +0100

    Added Icons; Refactoring

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ chrome.commands.onCommand.addListener(function (command) {
   if (command === "focus") {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {}, function (response) {
-        console.log(response.farewell);
+        // console.log(response.farewell);
       });
     });
   }
